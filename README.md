d7e2b47cced7fd18eab449a6def5a25b4ee3093e
MdNahid360 <mdnahid360s@gmail.com>
2021-07-08 05:24:38 -1200

    complete project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Shoping_card
+# Shopping Card
+##### simple java script shopping card.
+<img src="https://i.ibb.co/V2NFcnx/screencapture-127-0-0-1-5500-2021-07-08-05-14-35.png" width="100%"/>
+
+**Using : **HTML, CSS, BOOTSTRAP, JAVASCRIPT.
---
+++ index.js
@@ -0,0 +1,149 @@
+// document.getElementById('add').addEventListener('click', function (){
+//     hadletCounting(true);
+// })
+
+// document.getElementById('remove').addEventListener('click', function (){
+//     hadletCounting(false);
+// })
+
+// phone:::::::::
+// document.getElementById('phoneAdd').addEventListener('click', function(){
+//     const phoneInput = document.getElementById('phone-count');
+//     let phoneUpdate = parseInt(phoneInput.value);
+//      let totalPrice = phoneUpdate + 1;
+//      phoneInput.value = totalPrice;
+     
+//     let totalPhonePrice = totalPrice * 1219;
+//     document.getElementById('phoneTotal').innerText= '$' + totalPhonePrice;
+// });
+// document.getElementById('phoneRemove').addEventListener('click', function(){
+//     const phoneRemove = document.getElementById('phone-count');
+//     let removeUpdate = parseInt(phoneRemove.value);
+//     let total = removeUpdate - 1;
+//     phoneRemove.value = total;
+
+//     let totalRemovePrice = total * 1219;
+//     document.getElementById('phoneTotal').innerText = '$' + totalRemovePrice;
+// })
+
+// document.getElementById('phoneAdd').addEventListener('click', function(){
+//     handlePhone(true);
+// })
+
+// document.getElementById('phoneRemove').addEventListener('click', function(){
+//     handlePhone(false);
+// })
+function handleproduct(product ,productPass){
+    const phoneInput = document.getElementById(product+'-count');
+    let productUpdate = parseInt(phoneInput.value);
+    //  let totalPrice = phoneUpdate + 1;
+    let newProductUpdate = productUpdate;
+    if(productPass == true){
+          newProductUpdate = productUpdate + 1;
+    }
+    if(productPass == false && productUpdate > 0){
+        newProductUpdate = productUpdate  - 1;
+    }
+     phoneInput.value = newProductUpdate;
+     
+    // let totalProductPrice = newProductUpdate * 1219;
+    let totalProductPrice = 0;
+    if(product == 'phone'){
+        totalProductPrice = newProductUpdate*1219;
+    }
+    if(product == 'case'){
+        totalProductPrice=newProductUpdate*60;
+    }
+    document.getElementById(product+'-total').innerText= '$' + totalProductPrice;
+    totalPrice();
+}
+
+function totalPrice(){
+    // const phoneInput = document.getElementById('phone-count');
+    // const phoneCount = parseInt(phoneInput.value);
+    
+   const phoneCount = getInputValue('phone');
+   const caseCount = getInputValue('case');
+
+    const totalallPrice = phoneCount * 1219 + caseCount * 60;
+    document.getElementById('total-price').innerText = '$' + totalallPrice;
+
+    const tax = Math.round(totalallPrice / 100*10);
+    document.getElementById('tax-amount').innerText='$' + tax;
+
+    const grandTotal = totalallPrice + tax;
+    document.getElementById('grand-total').innerText='$' + grandTotal;
+
+}
+
+function getInputValue(productValue){
+    const  productInput = document.getElementById(productValue+'-count');
+    const productCount = parseInt(productInput.value);
+
+
+    return productCount;
+
+
+}
+
+document.getElementById('phoneClose').addEventListener('click', function(){
+   document.getElementById('phoneBox').style.display = 'none';
+})
+
+document.getElementById('hadPhonelose').addEventListener('click', function(){
+   document.getElementById('headphoneBox').style.display = 'none';
+})
+ 
+ 
+
+document.getElementById('checkOut').addEventListener('click', function(){
+    document.getElementById('card').style.display = 'none';
+})
+
+document.getElementById('checkOut').addEventListener('click', function(){
+    document.getElementById('blockCart').style.display = 'block';
+})
+
+document.getElementById('back').addEventListener('click', function(){
+    document.getElementById('blockCart').style.display = 'none';
+})
+
+
+document.getElementById('back').addEventListener('click', function(){
+    document.getElementById('card').style.display = 'block';
+});
+
+ // function handlePhone(phone){
+//     const phoneInput = document.getElementById('phone-count');
+//     let phoneUpdate = parseInt(phoneInput.value);
+//     //  let totalPrice = phoneUpdate + 1;
+//     let newPhoneUpdate = phoneUpdate;
+//     if(phone == true){
+//           newPhoneUpdate = phoneUpdate + 1;
+//     }
+//     if(phone == false && phoneUpdate > 0){
+//         newPhoneUpdate = phoneUpdate  - 1;
+//     }
+//      phoneInput.value = newPhoneUpdate;
+     
+//     let totalPhonePrice = newPhoneUpdate * 1219;
+//     document.getElementById('phoneTotal').innerText= '$' + totalPhonePrice;
+// }
+
+// // headPhone ::::::::
+
+// function hadletCounting (isIncres){
+//     let productInput = document.getElementById('product-count');
+//     let productPars = parseInt(productInput.value);
+//       let newCounting = productPars; 
+//        if(isIncres == true){
+//         newCounting =  productPars + 1;
+//        }
+//        if(isIncres == false && productPars > 0){
+//         newCounting = productPars - 1;
+//        }
+//     productInput.value = newCounting;
+
+//     let totalPrice = newCounting * 60;
+//     document.getElementById('case-total').innerText= '$' + totalPrice;
+// }
